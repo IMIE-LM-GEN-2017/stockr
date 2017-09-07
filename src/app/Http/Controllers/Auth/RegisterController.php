186a4b321b9593e8c085d10ced8ab39585d92099
Sbouyaud <Sophie.bouyaud2@gmail.com>
2017-09-07 15:37:31 +0200

    update

--- src/app/Http/Controllers/Auth/RegisterController.php
+++ src/app/Http/Controllers/Auth/RegisterController.php
@@ -49,7 +49,7 @@ class RegisterController extends Controller
     {
         return Validator::make($data, [
             'username' => 'required|string|max:255|unique:users',
-            'fist_name' => 'required|string|max:255',
+            'first_name' => 'required|string|max:255',
             'last_name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:6|confirmed',
