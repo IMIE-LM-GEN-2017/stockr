82c3591dc137a91a62b51b0b1727990fce4df54d
Sbouyaud <Sophie.bouyaud2@gmail.com>
2017-09-07 15:56:21 +0200

    it's okay ! i made it so faster, and it works !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- src/app/User.php
+++ src/app/User.php
@@ -14,8 +14,7 @@ class User extends Authenticatable
      *
      * @var array
      */
-    protected $fillable = [
-        'name', 'email', 'password',
+    protected $fillable = ['username', 'first_name', 'last_name', 'email', 'phone', 'password'
     ];
 
     /**
