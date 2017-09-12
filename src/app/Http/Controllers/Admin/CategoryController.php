791b4dea1112703e95d7d01084b9ccc6931d30fe
Sbouyaud <Sophie.bouyaud2@gmail.com>
2017-09-12 13:53:51 +0200

    admin up

---
+++ src/app/Http/Controllers/Admin/CategoryController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Sophie
+ * Date: 12/09/2017
+ * Time: 11:26
+ */
+
+namespace App\Http\Controllers\Admin;
+
+
+class CategoryController
+{
+
+}
---
+++ src/app/Http/Controllers/Admin/SellProductionController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Sophie
+ * Date: 12/09/2017
+ * Time: 11:27
+ */
+
+namespace App\Http\Controllers\Admin;
+
+
+class SellProductionController
+{
+
+}
